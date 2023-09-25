81d224326105123574bd0320f5902819cad9c837
Daniel Mendez <silex93@gmail.com>
2023-09-24 22:51:23 -0600

    updated buildroot and changed commit has to latest

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'efd4dae60e980649df49b21279958a859a29f10d'
+AESD_ASSIGNMENTS_VERSION = '1f84553b319855e356f76a14e037b38f8ab3a590'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
